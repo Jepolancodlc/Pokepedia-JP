2fef2ffdcafed9bd16fe70ce9b3277c5eef0d371
John Polanco <76429837+Jepolancodlc@users.noreply.github.com>
2023-05-03 23:03:00 +0200

    Layout inicial

--- pokepediaJP/src/app/app.component.ts
+++ pokepediaJP/src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'pokepediaJP';
+  showFiller = false;
+
+  tiles: Tile[] = [
+  {text: 'One', cols: 4, rows: 1, color: 'lightblue'},
+  {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+  {text: 'Four', cols: 3, rows: 2, color: '#DDBDF1'},
+  ];
 }
+
--- pokepediaJP/src/app/app.module.ts
+++ pokepediaJP/src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +17,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
